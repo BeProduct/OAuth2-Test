463e42f4cc67fe033d0c4644643bb23e799abff9
Daniel Pak <danpak6@gmail.com>
2017-06-05 22:56:58 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using OAuth2.Helpers;
+
+namespace OAuth2
+{
+    class Program
+    {
+        static readonly string authUrl = "https://id.winks.io/ids";
+        static readonly string clientId = "#CLIENT_ID#";
+        static readonly string clientSecret = "#SECRET_CODE#";
+        static readonly string companyName = "#COMPANY_NAME#";
+        static readonly string callbackUrl = "http://localhost:55555/";
+        static string accessToken = "";
+
+        static void Main(string[] args)
+        {
+            accessToken = Connect.Token(authUrl, clientId, clientSecret, callbackUrl);
+            Console.WriteLine("Yay! You got an access token ;-)");
+            Console.ReadLine();
+        }
+    }
+}
